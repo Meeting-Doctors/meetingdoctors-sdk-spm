7d8ad4d15c8c31d8c7dcd25fb7de3e8007f85dab
Alvaro Ruiz <engineering@meetingdoctors.com>
2025-08-18 12:48:49 +0200

    chore: Prepare versión 8.1.3-beta.14

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         // MARK: - MeetingDoctorsSDK
         .binaryTarget(
             name: "MeetingDoctorsSDK",
-            url: "https://sdk-download.meetingdoctors.com/iOS/MeetingDoctorsSDK/8.1.3-beta.13/MeetingDoctorsSDK.xcframework.zip",
-            checksum: "97f94b8d96c08edd984c9278de11515b8eae8daa85b42856ad1411976a4705ef"
+            url: "https://sdk-download.meetingdoctors.com/iOS/MeetingDoctorsSDK/8.1.3-beta.14/MeetingDoctorsSDK.xcframework.zip",
+            checksum: "950b3f188f722a25f1b347bbfcbb9e40c85e9f9044f29f05830e191786f53418"
         ),
         .target(
             name: "MeetingDoctorsSDKWrapper",
