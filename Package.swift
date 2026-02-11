7e01a8545e375ed12c9c1478edb1325a20ecb67d
Alvaro Ruiz <engineering@meetingdoctors.com>
2026-02-11 11:38:10 +0100

    Version 9.6.3

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         // MARK: - MeetingDoctorsSDK
         .binaryTarget(
             name: "MeetingDoctorsSDK",
-            url: "https://sdk-download.meetingdoctors.com/iOS/MeetingDoctorsSDK/9.6.2/MeetingDoctorsSDK.xcframework.zip",
-            checksum: "8a30d70b6bbc8e661d74a5a576ce91ed87f717a4a6d9b7378352c7eca97064da"
+            url: "https://sdk-download.meetingdoctors.com/iOS/MeetingDoctorsSDK/9.6.3/MeetingDoctorsSDK.xcframework.zip",
+            checksum: "560b6d14689fb9c8188c07172d43042c666d034d9c3b7ac217e3a9c86981357e"
         ),
         .target(
             name: "MeetingDoctorsSDKWrapper",
